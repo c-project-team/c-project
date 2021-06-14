043e7d29d8f615969963a8dd197a6e83ec38588d
Abdallah-shehata <82621364+Abdallah-shehata@users.noreply.github.com>
2021-06-14 22:57:11 +0200

    Update abdallah.c

--- abdallah.c
+++ abdallah.c
@@ -1,1 +1,52 @@
 
+read();
+ void read(){
+    if((fptr=fopen("dada.txt","rb"))==NULL){
+        printf("can not open the file\n");
+        exit(3);}
+        while(fread(&item,sizeof(item),1,fptr)){
+
+
+                 ptr=( info *) malloc(sizeof(info));
+
+    if(top==NULL)
+    {
+                top=ptr ;
+                ptr->link=NULL;
+
+    }
+    else {
+                ptr->link=top;
+                top=ptr;}
+
+
+    top->code=item.code;
+    strcpy(top->name,item.name);
+    top->price=item.price;
+    top->number=item.number;
+
+
+        }
+        fclose(fptr);
+}
+void clear(){
+      fptr=fopen("dada.txt","wb");
+      fclose(fptr);
+
+}
+void storedata(){
+             if((fptr=fopen("dada.txt","wb"))==NULL){
+        printf("can not open the file\n");
+        exit(2);
+        }
+        ptr=top;
+   while (ptr!=NULL){
+      item.code=ptr->code;
+    strcpy(item.name,ptr->name);
+    item.price=ptr->price;
+    item.number=ptr->number;
+
+     fwrite(&item,sizeof(item),1,fptr);
+     ptr=ptr->link;
+}
+ fclose(fptr);
